d8f2df5823b88aafab8b171a292330411b8faec9
ivanpast <ivanpastorsanz@gmail.com>
2024-08-29 22:16:07 +0200

    new names in scripts

--- scripts/run_bin_binary.py
+++ scripts/run_bin_binary.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    os.system("streamlit run bin_optimizer/bin_binary_target.py")
+    os.system("streamlit run BinWizard/bin_binary_target.py")
 
 if __name__ == "__main__":
     main()
--- scripts/run_bin_continuous.py
+++ scripts/run_bin_continuous.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    os.system("streamlit run bin_optimizer/bin_continuous_target.py")
+    os.system("streamlit run BinWizard/bin_continuous_target.py")
 
 if __name__ == "__main__":
     main()
